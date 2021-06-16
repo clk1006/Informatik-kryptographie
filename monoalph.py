23898c3834694edd733b9b931e02f959a6d8b864
clk1006 <46888928+clk1006@users.noreply.github.com>
2021-06-16 11:40:08 +0200

    added mono alphabetic de-/encryption

---
+++ monoalph.py
@@ -0,0 +1,17 @@
+import libclk
+import sys
+alph=input("Bitte das verschlüsselte Alphabet in Kleinbuchstaben angeben: ")
+text=input("Bitte Klartext angeben: ")
+isEncoded=bool(int(input("0 => Entschlüsseln; 1=> Verschlüsseln: ")))
+alph=libclk.purify(libclk.uniquify(alph))
+if len(alph)!=26:
+    print("Nicht alle Buchstaben sind im Alphabet enthalten")
+    sys.exit(1)
+text=libclk.purify(text)
+geheimtext=""
+for i in text:
+    if isEncoded:
+        geheimtext+=alph[ord(i)-97]
+    else:
+        geheimtext+=chr(alph.find(i)+97)
+print(geheimtext)
