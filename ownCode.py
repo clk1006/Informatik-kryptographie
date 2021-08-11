7c281c95cce8ded4ea4c093313820e8b5ec34cd6
clk1006 <casparkiehn@icloud.com>
2021-08-11 17:14:12 +0200

    Encryption code practically works, further testing may be necessary

--- ownCode.py
+++ ownCode.py
@@ -1,8 +1,8 @@
 import libclk
 text=input("Bitte Text angeben: ")
 key=int(input("Bitte Schlüssel angeben: "))
-encoding=bool(int(input("0 => Entschlüsseln; 1=> Verschlüsseln: ")))
-if encoding:
+encrypting=bool(int(input("0 => Entschlüsseln; 1=> Verschlüsseln: ")))
+if encrypting:
     text=libclk.purify(text)
     chars=[]
     for char in text:
@@ -45,4 +45,43 @@ if encoding:
             text+=chr(char+97)
     print(text)
 else:
-    
+    chars=[]
+    for char in text:
+        if ord(char)<97:
+            chars.append(ord(char)-39)
+        else:
+            chars.append(ord(char)-97)
+    text=""
+    for char in chars:
+        char=str(bin(char))[2:]
+        while len(char)<5:
+            char="0"+char
+        text+=char
+    keyLen=len(str(bin(key)))-2
+    text=libclk.distribute2d(text,keyLen)
+    text2=[]
+    for block in text:
+        num=""
+        for entry in block:
+            num+=str(entry)
+        num=int(num,2)
+        text2.append(num)
+    text=""
+    for num in text2:
+        block=str(bin(num ^ key))[2:]
+        while len(block)<keyLen:
+            block="0"+block
+        text+=block
+    text=text[:len(text)-keyLen+5-((len(text)-keyLen)%5)]
+    chars=[]
+    text=libclk.distribute2d(text,5)
+    for block in text:
+        num=""
+        for entry in block:
+            num+=str(entry)
+        num=int(num,2)
+        chars.append(num)
+    text=""
+    for char in chars:
+        text+=chr(char+97)
+    print(text)
